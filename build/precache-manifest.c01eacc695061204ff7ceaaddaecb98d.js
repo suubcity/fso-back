a8a92f91ca702cb70c51094f7e44b093068c96e8
suubcity <suubcity@outlook.com>
2021-01-20 12:06:44 -0700

    Part 3c

---
+++ build/precache-manifest.c01eacc695061204ff7ceaaddaecb98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2936fb5c97f8b370de6e911df7bae822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5457f721659efae7da7",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "7bcdd682e5a000484a02",
+    "url": "/static/js/2.934ed777.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/static/js/2.934ed777.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5457f721659efae7da7",
+    "url": "/static/js/main.68329b03.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
