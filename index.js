3dc22ee1471e37a39e2e480278f508952d89ffdc
suubcity <suubcity@outlook.com>
2021-01-21 09:48:29 -0700

    part3-5

--- index.js
+++ index.js
@@ -27,14 +27,8 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
 
   const note = new Note({
     content: body.content,
@@ -42,9 +36,15 @@ app.post("/api/notes", (request, response) => {
     date: new Date(),
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      savedNote.toJSON();
+    })
+    .then((savedAndFormattedNote) => {
+      response.json(savedAndFormattedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/notes/:id", (request, response, next) => {
@@ -96,6 +96,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if ((error.name = "ValidationError")) {
+    return response.status(400).send({ error: error.message });
   }
 
   next(error);
