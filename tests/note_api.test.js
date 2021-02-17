0816c9ff41d88c439c4cc9bb2c91a4e62173d086
suubcity <suubcity@outlook.com>
2021-02-17 14:27:54 -0700

    Eliminating the try-catch

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,6 +5,7 @@ const app = require('../app');
 const api = supertest(app);
 
 const Note = require('../models/note');
+const { tsImportEqualsDeclaration } = require('@babel/types');
 
 beforeEach(async () => {
 	await Note.deleteMany({});
@@ -66,6 +67,36 @@ test('note without content is not added', async () => {
 	const notesAtEnd = await helper.notesInDb();
 
 	expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+});
+
+test('a specific note can be viewed', async () => {
+	const notesAtStart = await helper.notesInDb();
+
+	const noteToView = notesAtStart[0];
+
+	const resultNote = await api
+		.get(`/api/notes/${noteToView.id}`)
+		.expect(200)
+		.expect('Content-Type', /application\/json/);
+
+	const processedNoteToView = JSON.parse(JSON.stringify(noteToView));
+
+	expect(resultNote.body).toEqual(processedNoteToView);
+});
+
+test('a note can be deleted', async () => {
+	const notesAtStart = await helper.notesInDb();
+	const noteToDelete = notesAtStart[0];
+
+	await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+	const notesAtEnd = await helper.notesInDb();
+
+	expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+	const contents = notesAtEnd.map((r) => r.content);
+
+	expect(contents).not.toContain(noteToDelete.content);
 });
 
 afterAll(() => {
